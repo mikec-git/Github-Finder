a60a6d65535e2d83f58e35448118237d662d0c6e
Mike Choi <mike.choi@live.ca>
2018-09-19 15:56:14 -0700

    Added basic markup for the UI

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 // Search input
 const searchUser = document.querySelector('#searchUser');
+// Init
+const github = new Github();
+const ui = new UI();
 
 searchUser.addEventListener('keyup', (e) => {
     // Get input text
     const userText = e.target.value;
-    const github = new Github();
 
     if(userText !== ''){
         github.getUser(userText)
         .then(data => {
             if(data.profile.message === 'Not Found'){
                 // Show alert
-                
+
             } else {
                 // Show profile
+                ui.showProfile(data.profile);
 
             }
         });
+    } else {
+        // Clear profile
     }
 });
