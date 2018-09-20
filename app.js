ee9706174eb1fd511318d263ea863c9c27929a74
Mike Choi <mike.choi@live.ca>
2018-09-19 17:12:10 -0700

    Added show repos functionality and outputs to ui

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ searchUser.addEventListener('keyup', (e) => {
     if(userText !== ''){
         github.getUser(userText)
         .then(data => {
-            if(data.profile.message === 'Not Found' && !document.querySelector('.alert.alert-danger')){
+            if(data.profile.message === 'Not Found'){
                 // Show alert
                 ui.showAlert('User not found', 'alert alert-danger');
             } else {
                 // Show profile
                 ui.showProfile(data.profile);
-
+                ui.showRepos(data.repos);
             }
         });
     } else {
